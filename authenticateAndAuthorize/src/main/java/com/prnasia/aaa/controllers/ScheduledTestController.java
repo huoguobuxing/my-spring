6060426945757b49d036cb669f945b6157272116
zhenli.liu <zhenli.liu@prnasia.com>
2019-03-21 18:49:05 +0800

    加入测试框架貌似不能运行

--- authenticateAndAuthorize/src/main/java/com/prnasia/aaa/controllers/ScheduledTestController.java
+++ authenticateAndAuthorize/src/main/java/com/prnasia/aaa/controllers/ScheduledTestController.java
@@ -5,13 +5,18 @@ import java.util.Date;
 
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController("test")
 public class ScheduledTestController {
 	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
 	@Scheduled(cron = "3 * * * * ?")
 	public void testTasks() {
 		System.out.println("定时任务执行时间：" + dateFormat.format(new Date()));
+	}
+	
+	public void test() {
+		
 	}
 }
---
+++ authenticateAndAuthorize/src/test/java/com/prnasia/aaa/test/controllers/ScheduledTestControllerTest.java
@@ -0,0 +1,22 @@
+package com.prnasia.aaa.test.controllers;
+
+
+
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ScheduledTestControllerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
